8599831d89b77112409d9598796a3ecf41c96351
Timofey Jedi <Timofey Jedi>
2016-03-01 10:13:50 +0300

    Added fadein and out effectsto the header

--- public/js/my_scripts.js
+++ public/js/my_scripts.js
@@ -8,7 +8,25 @@ $(document).ready(function(){
 		$('.site-header').css({
 			"transform": "translate(0%, -" + st/5 +"%"
 			});
+
+		if (st > 10) {
+			$('.header-nav').css({
+				"opacity": "0",
+				"transition": "0.8s",
+				"visibility": "hidden"
+			});
+		} else {
+			$('.header-nav').css({
+				"opacity": "1",
+				"transition": "0.5s",
+				"visibility": "visible"
+			});
+		}
+		console.log(st);
+
 	});
+
+
 
 });
 
